946abed4ebf6cdb3fba37f3f47dc7da8bd5d5541
Afiq <2000476@sit.singaporetech.edu.sg>
2022-11-15 21:14:32 +0800

    uppdated

---
+++ src/result.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form method="POST" action="index.php">
+            <?php echo $_POST["searchItem"]?><BR>
+            <button type="submit">Return</button>
+        </form>
+    </body>
+</html>
