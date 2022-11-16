b0eb611dcdc1ef9a8632ae1336f992c6f9ecde3b
afiqbusari7 <71884750+afiqbusari7@users.noreply.github.com>
2022-11-16 12:01:46 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 5000:80 --name my-apache-php-app -v /var/jenkins_home/workspace/Seleniumtest/src:/var/www/html --net test4net php:7.2-apache
+docker run -d -p 5000:80 --name my-apache-php-app -v /var/jenkins_home/workspace/Seleniumtest/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
